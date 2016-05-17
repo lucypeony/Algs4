fae5f51073647ea31e1be14a3affc30a4bec49ae
lucypeony <lucypeony@gmail.com>
2016-05-17 21:35:00 +0800

    using regex to parse HTML

--- duke/WebLinks.java
+++ duke/WebLinks.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.duke.*;
+import java.util.regex.*;
 
 public class WebLinks{
  
@@ -28,13 +29,26 @@ public class WebLinks{
  }
  
  
- public static void getContent(String url){
+ public static String getContent(String url){
      URLResource urllink=new URLResource(url);
+     /*
         for(String lines:urllink.lines()){
             StdOut.println(lines);
         }
-            
+        */
+     String s=urllink.asString();
+     return s ;
  }
+ 
+ public static void getTag(String content){
+     Pattern p=Pattern.compile("<a href='(.*?)'>");
+     Matcher m=p.matcher(content);
+     while(m.find()){
+     // System.out.println(m.group(0));
+      System.out.println(m.group(1));    
+     }
+ }//function
+ 
  
     public static void main(String[] args){
         /*
@@ -51,7 +65,7 @@ public class WebLinks{
         }//for 
         */
         
-        getContent("http://www.ielts-blog.com/ielts-writing-samples/ielts-essays-band-8/ielts-essay-topic-computers-instead-of-teachers/");
-        
+        String s=getContent("http://www.ielts-blog.com/ielts-writing-samples/ielts-essays-band-8/ielts-essay-topic-computers-instead-of-teachers/");
+        getTag(s);
     }//main
 }
