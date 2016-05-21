47c2fb10445c87935c2d83493ef488e25a277af1
lucypeony <lucypeony@gmail.com>
2016-05-21 14:12:57 +0800

    HourValue

---
+++ hackerrank/IPAddress.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class IPAddress {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=Integer.parseInt(sc.nextLine());
+        while(n--){
+            String s=sc.nextLine();
+            Pattern p=Pattern.compile("");
+            Matcher m=p.matcher(s);
+        }//while 
+        
+    }//main
+}//class 
--- leetcode/Leet349.java
+++ leetcode/Leet349.java
@@ -1,12 +1,21 @@
 public class Leet349{
     
+    public int bin_search(in
+    
     
     /**
      * Given two arrays,return their intersection 
      * Each element in the result must be unique ,and can be in any order 
      */
     public int[] intersection(int[] nums1,int[] nums2){
+        //sort two arrays
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         
+        for(int i=0;i<length(nums1);i++){
+            bin_search(nums2,0,length(nums2)-1);
+            
+        }//for
     }//intersection 
    
 }//class
