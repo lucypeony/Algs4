2fccf5520621cfb83ded2c2d807270a67dd8324e
lucypeony <lucypeony@gmail.com>
2016-06-01 11:44:46 +0800

    liang /hackerrank

---
+++ hackerrank/BST.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class BST{
+
+
+public static int getHeight(Node root){
+      //Write your code here
+   if(root==null)
+    return 0;
+   
+   int left=0,right=0;
+   
+   if(root.right!=null)
+       right=getHeight(root.right)+1;
+  if(root.left!=null)
+       left=getHeight(root.left)+1;
+   return left>right?left:right;
+    }
+
+ 
+ public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        } 
+}
